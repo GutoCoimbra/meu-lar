22bf04464f7316599786684b1b8279642af7e8d4
GutoCoimbra <gutembergcoimbra@hotmail.com>
2024-09-08 07:08:01 -0300

    fix

--- pages/api/units/index.ts
+++ pages/api/units/index.ts
@@ -32,7 +32,6 @@ export default async function handler(
         maintenanceFee,
         lastMaintenanceDate,
         imgUrl,
-        securityFeatures,
         accessInstructions,
         documents,
         averageRating,
