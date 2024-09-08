4d14b3ccf133466de171acace7b01f8477a51ae7
GutoCoimbra <gutembergcoimbra@hotmail.com>
2024-09-08 17:43:35 -0300

    Filter

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -80,7 +80,7 @@ const Filter: React.FC<FilterProps> = React.memo(({ onFilter, units }) => {
 
   return (
     <div className="p-4 bg-white shadow-md rounded mb-6 max-w-[1024px] mx-auto">
-      <span className="font-bold text-xl">
+      <span className="font-bold text-lg">
         Encontre um lar para chamar de seu
       </span>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
