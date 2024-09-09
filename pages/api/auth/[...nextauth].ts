74cece0a657aca8ed2e368af46d0775becf38812
GutoCoimbra <gutembergcoimbra@hotmail.com>
2024-09-09 00:38:53 -0300

    Configurar API para integração com Supabase e ajustar conexão com PostgreSQL

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,35 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // Outros provedores, se houver
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // Adicione outros provedores que você está utilizando
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  database: process.env.DATABASE_URL, // Se você estiver utilizando um banco de dados para sessões
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist o token OAuth no token JWT
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Envia o token ao client-side via a sessão
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
