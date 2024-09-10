b6c2e3a79bb3d94d728f3031acf6f873b1046b36
GutoCoimbra <gutembergcoimbra@hotmail.com>
2024-09-10 20:56:15 -0300

    fix: ajustment Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -80,6 +80,12 @@ const Header: React.FC = () => {
                   >
                     Meus Dados
                   </Link>
+                  <Link
+                    href="/meus-dados"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Meus Favoritos
+                  </Link>
                   <button
                     onClick={() => signOut()}
                     className="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
